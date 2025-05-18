250b4463854a9a476330b4127918492aa7ab7d1b
Fred <huertaserick@yahoo.ie>
2025-05-18 19:50:47 +0100

    feat: Footer component made and added also flex-1 used to take up whole space in main tag

---
+++ components/Footer.tsx
@@ -0,0 +1,27 @@
+const Footer = () => {
+  return (
+    <footer className="bg-surface text-textMuted text-sm py-6 mt-auto border-t border-slate-800">
+      <div className="container mx-auto px-4 flex flex-col sm:flex-row justify-between items-center gap-2">
+        <p>
+          &copy; {new Date().getFullYear()}{" "}
+          <span className="text-primary font-semibold">
+            Movie <span className="text-emerald-500">Explorer</span>
+          </span>
+          . All rights reserved.
+        </p>
+        <p className="italic">
+          Built by{" "}
+          <a
+            className="text-emerald-500 transition-all duration-200 cursor-pointer hover:text-emerald-600"
+            href="https://github.com/FredH2O"
+            target="_blank"
+          >
+            Frederico
+          </a>
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
