5f0b7c074412a44987a98493bec7f7921bc53ae2
Fred <huertaserick@yahoo.ie>
2025-05-22 01:45:22 +0100

    feat: add bin logic for removing items

--- components/Favourites.tsx
+++ components/Favourites.tsx
@@ -1,17 +1,28 @@
 "use client";
 
 import { useSearch } from "@/context/SearchContext";
-import { X } from "lucide-react";
+import { X, Trash2 } from "lucide-react";
 
 const Favourites = () => {
-  const { favourites, setOpenFavourites } = useSearch();
+  const { favourites, setFavourites, setOpenFavourites } = useSearch();
+
+  const handleRemove = (title: string, released: string) => {
+    setFavourites(
+      favourites.filter(
+        (fav) => fav.title !== title || fav.released !== released
+      )
+    );
+  };
 
   return (
     <div
       onClick={() => setOpenFavourites(false)}
       className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-80 p-4"
     >
-      <div className="relative bg-white text-black rounded-lg shadow-lg max-w-3xl w-full p-6 space-y-6 overflow-y-auto max-h-[90vh]">
+      <div
+        onClick={(e) => e.stopPropagation()}
+        className="relative bg-white text-black rounded-lg shadow-lg max-w-3xl w-full p-6 space-y-6 overflow-y-auto max-h-[90vh]"
+      >
         <button
           type="button"
           onClick={() => setOpenFavourites(false)}
@@ -30,13 +41,23 @@ const Favourites = () => {
           <ul className="space-y-4">
             {favourites.map((favourite) => (
               <li
-                key={favourite.title}
+                key={favourite.title + favourite.released}
                 className="flex flex-col md:flex-row md:justify-between md:items-center border-b pb-2"
               >
-                <h3 className="text-xl font-semibold">{favourite.title}</h3>
-                <p className="text-gray-600 italic">
-                  Released: {favourite.released}
-                </p>
+                <div>
+                  <h3 className="text-xl font-semibold">{favourite.title}</h3>
+                  <p className="text-gray-600 italic">
+                    Released: {favourite.released}
+                  </p>
+                </div>
+                <button
+                  onClick={() =>
+                    handleRemove(favourite.title, favourite.released)
+                  }
+                  className="mt-2 flex gap-1 cursor-pointer md:mt-0 text-red-500 hover:text-red-700 transition-colors"
+                >
+                  <Trash2 size={20} /> Delete
+                </button>
               </li>
             ))}
           </ul>
